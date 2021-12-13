e94a69b5879ae17f4a5c5e60a2d544e566e4f976
Ijay Abby <inyangasi4@gmail.com>
2021-12-13 15:42:20 +0300

    Style the navbar section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React from 'react';
 import './App.css';
-import LandingPage from './components/LandingPage'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <header className="App-header">
+        <NavBar />
+      </header>
     </div>
   );
 }
 
 export default App;
+
